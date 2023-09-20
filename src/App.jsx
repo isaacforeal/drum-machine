a38a9f07b7b486cd15056f00355a36230e9796b3
isaacforeal <isaacngabirano1@gmail.com>
2023-09-20 15:59:35 +0300

    first cl commit

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+
+import React from "react";
+
+
+
+
+
+
+export default function App(){
+const [activeKey,setActiveKey]=React.useState('')
+React.useEffect(()=>{
+    document.addEventListener('keydown',(event)=>{
+      playSound(event.key.toUpperCase())
+    },[])
+})
+   
+
+
+    const drumPads = [
+        {
+          keyCode: 81,
+          text: "Q",
+          src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+        },
+        {
+          keyCode: 87,
+          text: "W",
+          src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+        },
+        {
+          keyCode: 69,
+          text: "E",
+          src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+        },
+        {
+          keyCode: 65,
+          text: "A",
+          src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+        },
+        {
+          keyCode: 83,
+          text: "S",
+          src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+        },
+        {
+          keyCode: 68,
+          text: "D",
+          src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+        },
+        {
+          keyCode: 90,
+          text: "Z",
+          src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+        },
+        {
+          keyCode: 88,
+          text: "X",
+          src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+        },
+        {
+          keyCode: 67,
+          text: "C",
+          src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+        }
+      ];
+      
+  
+
+function playSound(selector){
+    const audio=document.getElementById(selector)
+    audio.play()
+    setActiveKey(selector)
+}
+
+
+
+
+     
+    return(
+        <div className="app">
+            <div id="drum-machine">
+                <div id="display">{activeKey}</div>
+                 <div className="drumPads">
+                     {
+                        drumPads.map((drumPad)=>(
+                            <div onClick={()=>{
+                                playSound(drumPad.text)                               
+                                
+                            }}
+                            className="drum-pad"
+                            id={drumPad.src}
+                            key={drumPad.text}
+                            >
+                                {drumPad.text}
+                                <audio className="clip" id={drumPad.text} src={drumPad.src}></audio>
+                            </div>
+                        ))
+                     }
+                 </div>              
+            </div>
+        </div>
+    )
+
+   
+
+}
